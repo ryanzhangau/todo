2cad7846d7bd6432cc42ec107ca4ecc44d905991
Ryan Zhang <ryanzhangau@gmail.com>
2019-06-05 22:19:56 +1000

    todo actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoContext from './TodoContext';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import { TodoHead } from './style/Todo.style';
+import SortByAlphaIcon from '@material-ui/icons/SortByAlpha';
 
 function App() {
-  return <div className='App' />;
+  const [todos, setTodos] = useState([
+    { id: 1, label: 'some task', complete: false, important: false }
+  ]);
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const complete = id => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) return { ...todo, complete: !todo.complete };
+        return todo;
+      })
+    );
+  };
+
+  const important = id => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) return { ...todo, important: !todo.important };
+        return todo;
+      })
+    );
+  };
+
+  return (
+    <>
+      <TodoHead>
+        <h3 style={{ margin: '0' }}>Todo List</h3>
+        <span style={{ gridColumn: '2 / span 2' }}>
+          {todos.filter(todo => !todo.complete).length} / {todos.length}
+        </span>
+      </TodoHead>
+      <TodoContext.Provider value={{ todos, setTodos, deleteTodo, complete, important }}>
+        <TodoList />
+        <AddTodo />
+      </TodoContext.Provider>
+    </>
+  );
 }
 
 export default App;
